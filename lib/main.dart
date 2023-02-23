46da0a7700ae7aaf36c4097a6472eec9cb885558
Daniel Llanos <danielagustinllanos8@gmail.com>
2023-02-23 19:23:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,391 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: 350,
+                height: 400,
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Container(
+                            width: 300,
+                            height: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(200),
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    CustomCard(),
+                  ],
+                ),
+              ),
+              const FechData(),
+              const VentasMarcas(),
+              const FechDataw(),
+              const CustomProductTile(
+                textoPrecio: '-\$222',
+                textoSubtitulo: 'ApleStore',
+                textoTitulo: 'Apple',
+                path: 'assets/apple.png',
+                colorcontainer: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          width: 350,
+          height: 400,
+          decoration: BoxDecoration(
+            color: Colors.amber.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: Row(
+                  children: const [
+                    Text(
+                      'Your Balance',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      '\$2,500',
+                      style: TextStyle(fontWeight: FontWeight.w800, fontSize: 40),
+                    ),
+                    Icon(
+                      Icons.ac_unit_outlined,
+                      size: 40,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('Profit'),
+                          Text(
+                            '+\$ 3,500',
+                            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 50,
+                      height: 50,
+                    ),
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('Spend'),
+                          Text(
+                            '-\$ 2,500',
+                            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FechDataw extends StatelessWidget {
+  const FechDataw({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 360,
+      height: 30,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Text(
+              'November 4',
+              style: TextStyle(color: Colors.grey.shade600, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FechData extends StatelessWidget {
+  const FechData({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 360,
+      height: 65,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 200,
+              height: 65,
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        'NOVEMBER 2020',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.arrow_drop_down)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'November 6',
+                        style: TextStyle(color: Colors.grey.shade600, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 114,
+              height: 65,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(color: Colors.grey.shade600),
+                  ),
+                  const Text(
+                    '-\$740',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VentasMarcas extends StatelessWidget {
+  const VentasMarcas({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          CustomProductTile(
+            textoPrecio: '-\$222',
+            textoSubtitulo: 'Sportswear',
+            textoTitulo: 'Nike',
+            path: 'assets/nike.png',
+            colorcontainer: Colors.yellow,
+          ),
+          CustomProductTile(
+            textoPrecio: '-\$400',
+            textoTitulo: 'Apple',
+            textoSubtitulo: 'Online store',
+            path: 'assets/apple.png',
+            colorcontainer: Colors.white,
+          ),
+          CustomProductTile(
+              textoPrecio: '-\$10',
+              textoTitulo: 'Coffee',
+              textoSubtitulo: 'Coffee House',
+              path: 'assets/coffee.png',
+              colorcontainer: Colors.white)
+        ],
+      ),
+    );
+  }
+}
+
+class CustomProductTile extends StatelessWidget {
+  const CustomProductTile({
+    Key? key,
+    required this.textoPrecio,
+    required this.textoTitulo,
+    required this.textoSubtitulo,
+    required this.path,
+    required this.colorcontainer,
+  }) : super(key: key);
+  final String textoPrecio;
+  final String textoTitulo;
+  final String textoSubtitulo;
+  final String path;
+  final Color colorcontainer;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Center(
+        child: Container(
+          width: 360,
+          height: 65,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.grey[300],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: 150,
+                  height: 65,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: colorcontainer,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            path,
+                            width: 25,
+                            height: 25,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            textoTitulo,
+                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+                          ),
+                          Text(
+                            textoTitulo,
+                            style: const TextStyle(color: Colors.black38),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(textoPrecio),
+                  ],
+                ),
+                const SizedBox(
+                  width: 15,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
